604e4b31fa1dafc73e1a0401dd28f0336a0498de
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2022-06-30 16:23:24 +0800

    Added stuff for scoring.

--- server/Machine Project Server/Assets/Scripts/ScoreManager.cs
+++ server/Machine Project Server/Assets/Scripts/ScoreManager.cs
@@ -1,3 +1,4 @@
+using RiptideNetworking;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,4 +18,18 @@ public class ScoreManager : MonoBehaviour
 
     public int playerOneScore = 0;
     public int playerTwoScore = 0;
+
+    public void UpdateScore(bool isPlayerOne)
+    {
+        if (isPlayerOne) playerOneScore += 1;
+        else playerTwoScore += 1;
+
+        SendScore();
+    }
+
+    // Message Sending function
+    private void SendScore()
+    {
+        
+    }
 }
